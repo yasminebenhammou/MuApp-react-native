cb0ccb01f8c995c391c4b3f4c61ec9d1e8038764
yasminebenhammou <yasmine.benhammou@student.ehb.be>
2023-02-03 12:29:19 +0100

    last config remove env add to other files

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
+import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer} from '@react-navigation/native'
 import  HomeScreen from './src/pages/HomeScreen';
-import * as React from 'react';
 import  MapScreen from './src/pages/MapScreen';
 import BottomMenu from './src/pages/BottomMenu';
 import { MapView } from '@situm/react-native-wayfinding';
